2fa680a8096afb77eba4a7d3ae06a3a49a69b348
Anderson Arboleya <anderson@arboleya.me>
2015-04-23 17:04:10 -0300

    test should run in client and server

--- test/meteor.js
+++ test/meteor.js
@@ -1,7 +1,5 @@
 if('undefined' !== typeof Tinytest){
 
-  if(Meteor.isServer) return;
-
   Tinytest.add('Ways', function (test) {
     test.isNotNull(Ways, {
       message: 'Expect `Ways` to be defined'

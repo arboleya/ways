ee50ffe59126383cd225d651bded6fc9c1bd15a4
Anderson Arboleya <anderson@arboleya.me>
2015-04-23 17:12:31 -0300

    refactoring everything

--- meteor.js
+++
@@ -1,6 +0,0 @@
-// exporting for meteor, see `package.js`
-Ways = this.Ways;
-delete this.Ways;
-delete this.Way;
-delete this.Flow;
-delete this.Fluid;
--- package.js
+++ package.js
@@ -3,26 +3,25 @@ var name = 'arboleya:ways';
 Package.describe({
   name: 'arboleya:ways',
   version: '0.4.0',
-  summary: 'Fluid router specially designed for complex UI animations and page transitions',
+  summary: 'Fluid router specially designed for modular UI animations and complex page transitions',
   git: 'https://github.com/arboleya/ways',
   documentation: 'README.md'
 });
-7
+
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use('arboleya:happens@0.6.0');
-  api.use('arboleya:ways-addressbar@0.2.0');
-  api.addFiles('lib/ways.js', 'client');
-  api.addFiles('lib/way.js', 'client');
-  api.addFiles('lib/flow.js', 'client');
-  api.addFiles('lib/fluid.js', 'client');
-  api.addFiles('meteor.js', 'client');
-  api.export('Ways', 'client');
+  api.use('arboleya:ways-addressbar@0.2.1');
+  api.export('Ways');
+  api.addFiles('lib/fluid.js');
+  api.addFiles('lib/flow.js');
+  api.addFiles('lib/way.js');
+  api.addFiles('lib/ways.js');
 });
 
 Package.onTest(function (api) {
   api.use(name);
   api.use('tinytest');
-  api.use('arboleya:ways', 'client');
+  api.use('arboleya:ways');
   api.addFiles('test/meteor.js');
 });

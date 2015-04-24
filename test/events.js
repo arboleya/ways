2390a1912f1fd2e812e76410a315b57ecc9e6a94
Anderson Arboleya <anderson@arboleya.me>
2015-04-24 19:26:13 -0300

    Merge remote-tracking branch 'giuliandrimba/feature/router-change-event'
    Adding events support for Ways

---
+++ test/events.js
@@ -0,0 +1,31 @@
+var ways = require('../lib/ways');
+var should = require('chai').should();
+
+describe('[events]', function(){
+
+
+  before(function(){
+    ways.reset();
+    ways('/pages', function(){});
+  });
+
+  var url_changed;
+  function url_change(url){
+    url_changed = url;
+  }
+
+  it('should dispatch an event when the url changes', function(done) {
+    ways.on('url:change', url_change);
+    ways.go('/pages');
+    url_changed.should.equal('/pages');
+    done();
+  });
+
+  it('should not dispatch event after listener is removed', function(done) {
+    url_changed = 'none';
+    ways.off('url:change');
+    ways.go('/pages');
+    url_changed.should.equal('none');
+    done();
+  });
+});
